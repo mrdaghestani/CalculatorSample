59df6757dd2adb4bfaf728f3a59c82054f8e98b6
mrdaghestani <mrdaghestani@gmail.com>
2020-07-20 17:51:52 +0430

    Add project files.

---
+++ CalculatorSample/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CalculatorSample.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
